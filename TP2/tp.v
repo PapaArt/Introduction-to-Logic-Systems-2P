3f0d7c16928ffac10c091ef0a036583af54f5b97
lucianobajr <luciano.alcantara@ufv.br>
2019-11-10 15:32:40 -0300

    Update tp.v

--- TP2/tp.v
+++ TP2/tp.v
@@ -7,3 +7,23 @@ modulo tp(clk,reset,ok,tom,nota,fim,tipo,display);
 	output reg [1:0] tipo;
 	output [6:0] display;
     Alfa(Tom, notas1, notas2, notas3, saida1, saida2, saida3, saida4, saida5, saida6, saida7);
+
+	def[4:0]     		    estado_inicial   = 5'b00000,
+							estado_nota1     = 5'b00001,
+							estado_nota2_op1 = 5'b00010,
+							estado_nota2_op2 = 5'b00011,
+							estado_nota2_op3 = 5'b00100,
+							estado_nota3_op1 = 5'b00101,
+							estado_nota3_op2 = 5'b00110,
+							estado_nota3_op3 = 5'b00111,
+							estado_nota4_op1 = 5'b01000,
+							estado_nota4_op2 = 5'b01001,
+							estado_nota4_op3 = 5'b01010,
+							estado_nota5_op1 = 5'b01011,
+							estado_nota5_op2 = 5'b01100,
+							estado_nota5_op3 = 5'b01101,
+							estado_final_op1 = 5'b01110,
+							estado_final_op2 = 5'b01111,
+							estado_final_op3 = 5'b10000,
+							estado_erro      = 5'b10001;
+    
