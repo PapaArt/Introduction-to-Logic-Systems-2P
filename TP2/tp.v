f34a4f2507a0f99c4c4b6f2cc6ce748596eb7e04
Luciano Belo <luciano.alcantara@ufv.br>
2019-11-11 12:17:49 -0300

    7

--- TP2/tp.v
+++ TP2/tp.v
@@ -1,5 +1,5 @@
 /*Trabalho Maquina de Estado Finito-Módulo Adjetivo e Advérbio*/
-
+`include "Alfa_desc.v"
 module tp(clk,reset,ok,tom,nota,fim,tipo,display);
     input ok, tom, reset, clk;
 	input [2:0] nota;
@@ -8,12 +8,18 @@ module tp(clk,reset,ok,tom,nota,fim,tipo,display);
 	output [6:0] display;
 
     Alfa_desc Adverbio(Tom, notas1, notas2, notas3, saida1, saida2, saida3, saida4, saida5, saida6, saida7);
+	
+	initial begin
+		$dumpfile("onda.vcd");
+        $dumpvars(0, tp);
+        $monitor("%b%b%b%b saida = %b%b%b%b%b%b%b", tom, notas1, notas2, notas3, saida1, saida2, saida3, saida4, saida5, saida6, saida7);	
+	end
 
-	def[4:0]				estado_inicial    = 5'b00000,
-							estado_nota1      = 5'b00001,
-							estado_nota2_adj  = 5'b00010,
-							estado_nota2_comp = 5'b00011,
-							estado_nota2_adv  = 5'b00100,
+	localparam[4:0]			estado_inicial    = 5'b00000,
+	                        estado_nota1      = 5'b00001,
+	                        estado_nota2_adj  = 5'b00010, 
+ 	                        estado_nota2_comp = 5'b00011,
+	                        estado_nota2_adv  = 5'b00100,
 							estado_nota3_adj  = 5'b00101,
 							estado_nota3_comp = 5'b00110,
 							estado_nota3_adv  = 5'b00111,
@@ -29,7 +35,7 @@ module tp(clk,reset,ok,tom,nota,fim,tipo,display);
 							estado_erro       = 5'b10001;				
 										
     
-	def[2:0]                           nota_x =   3'b000,
+	localparam[2:0]                       nota_x =   3'b000,
 									      do  = 3'b001, 
 									      re  =	3'b010,		
 									      mi  =	3'b011,
@@ -38,60 +44,166 @@ module tp(clk,reset,ok,tom,nota,fim,tipo,display);
 									      la  = 3'b110,
 									      si  = 3'b111;
 
-	def[1:0]                       tipo_nulo = 2'b00,
+	localparam[1:0]                tipo_nulo = 2'b00,
 								    tipo_adj = 2'b01,
  								    tipo_comp= 2'b10,
 								    tipo_adv = 2'b11;
 
-	def                               final  = 1'b1;
+	localparam                    finalizado = 1'b1,
 							     em_processo = 1'b0;
 	
 	reg[4:0]estados=estado_inicial;
 
 	always @(posedge clk) begin
 		if(reset)
-			estatos=estado_inicial;
+			estados=estado_inicial;
 		else if(ok) begin
 			case(estados)
-				estado_inicial:begin
-					if(nota!=nota_x)
-						estados=estado_nota1;
-					else
-						estados=estado_erro;
-				end
+					estado_inicial:begin
+						if(nota!=nota_x)
+							estados=estado_nota1;
+						else
+							estados=estado_erro;						
+					end
 
-				estado_nota1:begin
-					if(nota!=nota_x)
-						estados=estado_nota2_adj;
-					else if((~tom && nota==la) ||(~tom && nota==si))
-						estados=estado_nota2_comp;
-					else if(~tom && nota==la)
-						estados=estado_nota2_adv;
-					else
-						estados=estado_erro;
-				end
+					estado_nota1:begin
+						if(nota!=nota_x)
+							estados=estado_nota2_adj;
+						else if(nota!=nota_x)
+							estados=estado_nota2_comp;
+						else if(nota!=nota_x)
+							estados=estado_nota2_adv;	
+						else
+							estados=estado_erro;
+					end
 
-				estado_nota2_adj:begin
-					if((~tom && nota==la) ||(~tom && nota==si))
-						estados=estado_nota3_adj;
-					else
-						estados=estado_erro;
-				end
+					estado_nota2_adj:begin
+						if(nota!=nota_x)
+							estados = estado_nota3_adj;
+						else
+							estados=estado_erro;
+					end
 
-				estado_nota2_comp:begin
-					if((tom && nota == do) || (tom && nota == re))
-						estados=estado_nota3_comp;
-					else
-						estados=estado_erro;
-				end
+					estado_nota2_comp:begin
+						if(nota!=nota_x)
+							estados = estado_nota3_comp;
+						else
+							estados=estado_erro;
+					end
 
-				estado_nota2_adv:begin
-					if(~tom && nota == si)
-						estados=estado_nota3_adv;
-					else
-						estados=estado_erro;
-				end
+					estado_nota2_adv:begin
+						if(nota!=nota_x)
+							estados = estado_nota3_adv;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota3_adj:begin
+						if(nota!=nota_x)
+							estados = estado_nota4_adj;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota3_comp:begin
+						if((~tom && nota == la) || (~tom && nota == si) )
+							estados = estado_nota4_comp;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota3_adv:begin
+						if(~tom && nota == la)
+							estados = estado_nota4_adv;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota4_adj: begin
+						if((~tom && nota == la) || (~tom && nota == si))
+							estados = estado_nota5_adj;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota4_comp:begin
+						if((tom&& nota == do ) ||(tom&& nota == re))
+							estados =estado_nota5_comp;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota4_adv:begin
+						if(~tom && nota == si)
+							estados=estado_nota5_adv;
+						else
+							estados=estado_erro;
+					end	
+
+					estado_nota5_adj: begin
+						if(nota == nota_x)
+							estados = estado_final_adj;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota5_comp:begin
+						if(nota == nota_x)
+							estados =estado_final_comp;
+						else
+							estados=estado_erro;
+					end
+
+					estado_nota5_adv:begin
+						if(nota == nota_x)
+							estados=estado_final_adv;
+						else
+							estados=estado_erro;
+					end	
+
+					estado_final_adj:begin
+						
+					end
+
+					estado_final_comp:begin
+						
+					end
+
+					estado_final_adv:begin
+						
+					end
+
+					default: estados = estado_inicial;
 			endcase
 		end
+	end
+
+	always @ (estados)begin
+		case(estados)
+			estado_erro:begin
+				fim = finalizado;
+				tipo = tipo_nulo; 
+			end
+
+			estado_final_adj:begin
+				fim = finalizado;
+				tipo = tipo_adj;
+			end
+
+			estado_final_comp:begin
+				fim = finalizado;
+				tipo = tipo_comp;
+			end
+
+			estado_final_adv:begin
+				fim = finalizado;
+				tipo = tipo_adv;
+			end
+
+			default:begin
+				fim=em_processo;
+				tipo=tipo_nulo;
+			end
+		endcase
 	end	
-endmodule
+endmodule	
